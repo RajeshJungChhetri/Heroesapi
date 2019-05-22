c24bfb887ab85a3b7747d24f0b2b9e26417f372d
Softwarica College Lab <softwarica@softwarica.com>
2019-05-22 11:55:51 +0545

    browse imaGE

--- app/src/main/java/com/heroesapi/MainActivity.java
+++ app/src/main/java/com/heroesapi/MainActivity.java
@@ -1,11 +1,22 @@
 package com.heroesapi;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
 
 import API.HeroesAPI;
 import Model.Heroes;
@@ -21,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText edHeroesName, edHeroesDesc;
     private Button btnSave;
+    private ImageView imgview;
+    String imagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,16 @@ public class MainActivity extends AppCompatActivity {
         edHeroesName = findViewById(R.id.edHeroesName);
         edHeroesDesc = findViewById(R.id.edHeroesDesc);
         btnSave = findViewById(R.id.btnSave);
+        imgview = findViewById(R.id.imgview);
+
+        imgview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BrowseImage();
+            }
+
+
+        });
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
                 String desc = edHeroesDesc.getText().toString();
 
 
-
                 Retrofit retrofit = new Retrofit.Builder()
                         .baseUrl(url.BASE_URL)
                         .addConverterFactory(GsonConverterFactory.create())
@@ -50,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
                 HeroesAPI heroesAPI = retrofit.create(HeroesAPI.class);
 
-                Call<Void> heroesCall = heroesAPI.addHero (name,desc);
+                Call<Void> heroesCall = heroesAPI.addHero(name, desc);
 
                 heroesCall.enqueue(new Callback<Void>() {
                     @Override
@@ -65,16 +87,57 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onFailure(Call<Void> call, Throwable t) {
-                        Toast.makeText(MainActivity.this, "Error" +t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Error" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
 
                     }
                 });
 
 
-
-
             }
         });
 
+    }
+
+    private void BrowseImage() {
+        Intent intent = new Intent((Intent.ACTION_PICK));
+        intent.setType("image/*");
+        startActivityForResult(intent, 0);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (data == null) {
+                Toast.makeText(this, "please select an image", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+        Uri uri = data.getData();
+        imagePath = getRealPathFromUri(uri);
+        previewImage(imagePath);
+    }
+
+    private void previewImage(String imagePath) {
+        File imgFile = new File(imagePath);
+        if (imgFile.exists()) {
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+            imgview.setImageBitmap(myBitmap);
+        }
+    }
+
+
+    private String getRealPathFromUri(Uri uri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(getApplicationContext(), uri, projection, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int colIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(colIndex);
+        cursor.close();
+        return result;
     }
 }
